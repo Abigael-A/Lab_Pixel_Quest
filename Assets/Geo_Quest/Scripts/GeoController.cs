ab12f681ba71a532d1743b56579d70967cd4380f
Abigael-A <abigael.candice@gmail.com>
2025-07-10 12:19:31 -0400

    7/10/2025 Stem Institute

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -1,26 +1,57 @@
+
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GeoController : MonoBehaviour
 {
-    int food = 3;
-    int var1 = 100;
+    private Rigidbody2D rb;
+    public int speed = 3;
     // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hit");
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Hit");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Hit");
+    }
     void Start()
     {
-        Debug.Log("Hello world");
-
-        int variabledos = 25;
-
-        print(var1 + variabledos);
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(food);
+       
 
-        food++;
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+        //Debug.Log(xInput);
+        /*
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }*/
     }
 }
