74b477b564fd3eb982a4303859dcb836673c6467
Abigael-A <abigael.candice@gmail.com>
2025-08-04 16:11:43 -0400

    yrfdf

--- Assets/Stand Clear/Abigael/barrier visiblity.cs
+++ Assets/Stand Clear/Abigael/barrier visiblity.cs
@@ -4,12 +4,30 @@ using UnityEngine;
 
 public class barriervisiblity : MonoBehaviour
 {
+    public string targetTag = "Enemy";
+
+    private Renderer objectRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        objectRenderer = GetComponent<Renderer>();
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(targetTag))
+        {
+            SetVisibility(false);
+        }
+    }
+
+    void SetVisibility(bool isVisible)
+    {
+        if (objectRenderer != null)
+        {
+            objectRenderer.enabled = isVisible;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
