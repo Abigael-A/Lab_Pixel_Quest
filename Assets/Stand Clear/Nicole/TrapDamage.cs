1701b8b24563ad883fb491d441064c25353dc474
Nicole Chen <nicolechen220@gmail.com>
2025-08-04 12:05:42 -0400

    ADDED TRAPS (NEED TO FIX)

--- Assets/Stand Clear/Nicole/TrapDamage.cs
+++ Assets/Stand Clear/Nicole/TrapDamage.cs
@@ -2,33 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyDamage: MonoBehaviour
+public class TrapDamage : MonoBehaviour
 {
-    private BarrierHealths BarrierHealth;
-    public int damage=2;
+    private EnemyHealth EnemyHealth;
+    public int damage = 2;
 
     public float timer = 3;
     private float current = 0;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position += Vector3.right * Time.deltaTime;
-    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Touch");
-        if (other.gameObject.tag == "Barrier") 
+        if (other.gameObject.tag == "Enemy")
         {
             Debug.Log("Attack");
-            other.gameObject.GetComponent<BarrierHealths>().TakeDamage(damage);
-        
+            other.gameObject.GetComponent<EnemyHealth>().TakeDamage(damage);
+
         }
     }
 
@@ -37,14 +27,13 @@ public class EnemyDamage: MonoBehaviour
         current -= Time.deltaTime;
         if (current < 0)
         {
-            collision.gameObject.GetComponent<BarrierHealths>().TakeDamage(damage);
+            collision.gameObject.GetComponent<EnemyHealth>().TakeDamage(damage);
             current = timer;
         }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        Debug.Log("death");
         Destroy(gameObject);
     }
 }
