0ef8a63f0b93871b93aa9e3f5f6b9904be9bcbb3
Abigael-A <abigael.candice@gmail.com>
2025-08-04 16:58:06 -0400

    dfghj

--- Assets/Stand Clear/Abigael/barrier visiblity.cs
+++ Assets/Stand Clear/Abigael/barrier visiblity.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class barriervisiblity : MonoBehaviour
 {
     public string targetTag = "Enemy";
+    
 
     private Renderer objectRenderer;
     // Start is called before the first frame update
